c8118a45c8c33b66357f1840b4a27545aed60a54
ryanamannion <ram321@georgetown.edu>
2020-12-04 20:08:02 -0500

    commits iterative improvements to makecents

--- makecents/actions/actions.py
+++ makecents/actions/actions.py
@@ -7,21 +7,36 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import glob
+from typing import Any, Text, Dict, List
+
+from pcgs_scraper import pcgs_query
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionReturnPrice(Action):
+
+    # def __init__(self):
+    #     if os.path.isfile('')
+
+    def name(self) -> Text:
+        return "action_return_price"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        coin_query = tracker.get_latest_entity_values("coin")
+
+        validated_query = next(pcgs_query.validate_query(coin_query, verbose=False), None)
+
+        # cant query the price guide because it needs to load the table first,
+        # and I need to make the file system better for that
+        # TODO:
+        #   - use glob to get pcgs_price_guide files and write a function to
+        #     pick out the latest one using datetime
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
