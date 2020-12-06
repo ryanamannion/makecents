8939ad59b1637b7c592d7310b97a074207bcf836
ryanamannion <ram321@georgetown.edu>
2020-12-06 01:06:19 -0500

    iterative commit to improve dialogue agent

--- makecents/actions/actions.py
+++ makecents/actions/actions.py
@@ -7,12 +7,45 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+import os
 import glob
+from datetime import datetime, date, time
+import pickle
 from typing import Any, Text, Dict, List
 
-from pcgs_scraper import pcgs_query
+from pcgs_scraper import pcgs_query, utils
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+
+
+def most_recent_guide():
+    pcgs_files = glob.glob('./data/pcgs_price_guide*')
+
+    file_datetimes = []
+
+    for pcgs_file in pcgs_files:
+        # file is format: pcgs_price_guide-YYYYMMDD-HHMMSS.pkl
+        # .pkl is [-4:]
+        date_format = pcgs_file[-19:-4]
+        year = int(date_format[:4])
+        month = int(date_format[4:6])
+        day = int(date_format[6:8])
+        # skip hyphen
+        hour = int(date_format[9:11])
+        minute = int(date_format[11:13])
+        second = int(date_format[13:15])
+        d = date(year, month, day)
+        t = time(hour, minute, second)
+        this_file_datetime = datetime.combine(d, t)
+
+        file_datetime = (pcgs_file, this_file_datetime)
+        file_datetimes.append(file_datetime)
+
+    most_recent = max(file_datetimes, key=lambda x: x[1])
+
+    price_guide = pickle.load(open(most_recent[0], 'rb'))
+
+    return price_guide
 
 
 class ActionReturnPrice(Action):
@@ -27,16 +60,40 @@ class ActionReturnPrice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        coin_query = tracker.get_latest_entity_values("coin")
+        coin_entities = tracker.get_latest_entity_values("coin")
+        *_, last = coin_entities
 
-        validated_query = next(pcgs_query.validate_query(coin_query, verbose=False), None)
+        validated_query = pcgs_query.validate_query(last, verbose=False)
+        price_guide = most_recent_guide()
 
-        # cant query the price guide because it needs to load the table first,
-        # and I need to make the file system better for that
-        # TODO:
-        #   - use glob to get pcgs_price_guide files and write a function to
-        #     pick out the latest one using datetime
+        results = pcgs_query.query_price_guide(validated_query, price_guide)
 
-        dispatcher.utter_message(text="Hello World!")
+        if len(results) == 0:
+            msg = "I'm sorry, I could not find a coin matching your query."
+            dispatcher.utter_message(text=msg)
+            dispatcher.utter_message(template='utter_search_help')
+        elif len(results) == 1:
+            result = results[0]
+            prices = utils.price_table(result['desig'], result['prices'])
+            dispatcher.utter_message(text=prices)
+        elif len(results) > 1:
+            """
+            User query returns more than one coin result:
+                - tell user the results
+                - give user buttons
+                - buttons return exact description of coin, which should return
+                  just that coin with pcgs_query.query_price_guide()
+            """
+            dispatcher.utter_message("I found multiple results for your query.")
+            descriptions = [x['description'] for x in results]
+            buttons = []
+            for description in descriptions:
+                button = {
+                    "payload": description,
+                    "title": description
+                }
+                buttons.append(button)
+            dispatcher.utter_message("Please select the coin you would like to "
+                                     "inspect: ", buttons=buttons)
 
         return []
